78c257f2ccfb8c68da4e1a956f0bf55e24c6acac
shruti862 <shrutiyadav862@gmail.com>
2024-01-15 10:59:25 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,15 @@
-'use client'
-import React from 'react'
-import Link from 'next/link'
-
+"use client";
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
     <div>
-      <Link href={'/SignIn'}><h1>sign in</h1></Link>
+      <Link href={"/signIn"}>
+        <h1>sign in</h1>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
-
-
+export default page;
